5451cba33cce8d1eb079cf47ffae337ec7610c68
tonmoysaha0005 <tonmoy.saha@gtechcp.com>
2021-10-22 15:57:30 +0600

    initialize

---
+++ src/main/java/com/square/health/SquareHealthApplication.java
@@ -0,0 +1,13 @@
+package com.square.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquareHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquareHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/square/health/SquareHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.square.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquareHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
